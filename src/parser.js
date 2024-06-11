c1554636013534060ef8742119cb6fd3fcdcee50
McGrady <3260884+mcgradycchen@users.noreply.github.com>
2024-06-11 22:42:41 +0800

    add parse meta

--- src/parser.js
+++ src/parser.js
@@ -4,61 +4,171 @@
  *   This project is licensed under the Apache 2 License, see LICENSE
  */
 
+const metaTypeName = "@name";
+const metaTypeNote = "@note";
+const metaTypeNoRedirect = "@no-redirect";
+const metaTypeNoCookieJar = "@no-cookie-jar";
+const metaTypePrompt = "@prompt";
+const metaTypeComment = "@comment";
+
 const parser = () => {
   "use strict";
 
   var tokenizer = function (type, line) {
-
     let create = function (type, value) {
       let obj = { type: type, value: value };
       return obj;
     };
 
+    let isBlank = (char) => {
+      return char === " " || char === "\t" || char === "\r" || char === "\n";
+    };
+
+    let isMetaType = (start, end, metaType) => {
+      let len = metaType.length;
+      return (
+        ((end - start > len - 1 && isBlank(line.charAt(start + len))) || end - start === len - 1) &&
+        line.substring(start, start + len).toLowerCase() === metaType
+      );
+    };
+
+    let nextBlank = (start, end) => {
+      if (start > end) {
+        return end + 1;
+      }
+      while (start <= end && !isBlank(line.charAt(start))) {
+        ++start;
+      }
+      return start;
+    };
+
     let skipLeftBlank = (start, end) => {
-      while (start < end) {
-        if (
-          line.charAt(start) === " " ||
-          line.charAt(start) === "\t" ||
-          line.charAt(start) === "\r" ||
-          line.charAt(start) === "\n"
-        ) {
-          ++start;
-        }
+      if (start > end) {
+        return end + 1;
+      }
+      while (start < end && isBlank(line.charAt(start))) {
+        ++start;
       }
       return start;
     };
 
     let skipRightBlank = (start, end) => {
-      while (start < end) {
-        if (
-          line.charAt(end) === " " ||
-          line.charAt(end) === "\t" ||
-          line.charAt(end) === "\r" ||
-          line.charAt(end) === "\n"
-        ) {
-          --end;
-        }
+      if (start > end) {
+        return end + 1;
+      }
+      while (start < end && isBlank(line.charAt(end))) {
+        --end;
       }
       return end;
     };
-    let start = 0, end = line.length;
-    if (start < end) {
+
+    let start = 0,
+      end = line.length - 1;
+    if (start <= end) {
       start = skipLeftBlank(start, end);
       end = skipRightBlank(start, end);
       if (start === end) {
         return create(type, null);
       }
       if (type === "seperator" || type === "meta" || type === "var") {
-        if (source.charAt(start) === "#") {
+        if (line.charAt(start) === "#" || (line.charAt(start) === "/" && line.charAt(start + 1) === "/")) {
           // meta line
-          if (source.indexOf("###", start) === 0) {
+          console.log("1")
+          if (line.charAt(start) !== "#") {
+            ++start;
+          }
+          if (line.indexOf("###", start) === 0) {
             // new request block
             type = "seperator";
+            start = skipLeftBlank(start + 3, end);
+            return create(type, start >= end + 1 ? [] : [line.substring(start, end + 1)]);
+          } else if (start < end && isBlank(line.charAt(start + 1))) {
             start = skipLeftBlank(start + 1, end);
-            end = skipRightBlank(start, end);
-            return create(type, start === end ? [] : [line.substring(start, end)]);
+            if (isMetaType(start, end, metaTypePrompt)) {
+              // meta promt
+              type = "meta";
+              let value = [metaTypePrompt];
+              // parse prompt key
+              start += metaTypePrompt.length + 1;
+              start = skipLeftBlank(start, end);
+              let varStart = start;
+              start = nextBlank(start, end);
+              if (varStart < start) {
+                value.push(line.substring(varStart, start));
+              }
+              // parse prompt description
+              ++start;
+              start = skipLeftBlank(start, end);
+              if (start <= end) {
+                value.push(line.substring(start, end + 1));
+              }
+              return create(type, value);
+            } else if (isMetaType(start, end, metaTypeNote)) {
+              // meta note
+              type = "meta";
+              let value = [metaTypeNote];
+              // parse note description
+              start += metaTypeNote.length + 1;
+              start = skipLeftBlank(start, end);
+              if (start <= end) {
+                value.push(line.substring(start, end + 1));
+              }
+              return create(type, value);
+            } else if (isMetaType(start, end, metaTypeName)) {
+              // meta name
+              type = "meta";
+              let value = [metaTypeName];
+              // parse name key
+              start += metaTypeName.length + 1;
+              start = skipLeftBlank(start, end);
+
+              if (start <= end) {
+                value.push(line.substring(start, end + 1));
+              }
+              return create(type, value);
+            } else if (isMetaType(start, end, metaTypeNoRedirect)) {
+              // meta no redirect
+              type = "meta";
+              let value = [metaTypeNoRedirect];
+              // parse no redirect description
+              start += metaTypeNoRedirect.length + 1;
+              start = skipLeftBlank(start, end);
+              if (start <= end) {
+                value.push(line.substring(start, end + 1));
+              }
+              return create(type, value);
+            } else if (isMetaType(start, end, metaTypeNoCookieJar)) {
+              // meta no cookie jar
+              type = "meta";
+              let value = [metaTypeNoCookieJar];
+              // parse no cookie jar description
+              start += metaTypeNoCookieJar.length + 1;
+              start = skipLeftBlank(start, end);
+
+              if (start <= end) {
+                value.push(line.substring(start, end + 1));
+              }
+              return create(type, value);
+            } else {
+              type = "meta";
+              let value = [metaTypeComment, " "];
+              start = skipLeftBlank(start, end);
+
+              if (start <= end) {
+                value.push(line.substring(start, end + 1));
+              }
+              return create(type, value);
+            }
+          } else {
+            type = "meta";
+            let value = [metaTypeComment];
+            ++start
+            if (start <= end) {
+              value.push(line.substring(start, end + 1));
+            }
+            return create(type, value);
           }
-        } else if (source.charAt(start) === "@") {
+        } else if (line.charAt(start) === "@") {
           // variable line
         } else {
         }
@@ -77,14 +187,19 @@ const parser = () => {
     console.log(source);
     while (position < len) {
       let lineEnd = source.indexOf("\n", position);
-      console.log(lineEnd)
+      if (lineEnd == -1) {
+        lineEnd = source.length;
+      }
       try {
         let expr = tokenizer(type, source.substring(position, lineEnd));
         type = expr.type;
         exprs.push(expr);
-      } catch (error) {}
+      } catch (error) {
+        console.error(error);
+      }
       position = lineEnd + 1;
     }
+    return exprs;
   };
 
   return parser;
--- src/parser.test.js
+++ src/parser.test.js
@@ -1,8 +1,240 @@
 const parser = require("./parser")();
 
-describe("parser", function () {
-  it("parser seperator", function () {
+describe("parser seperator", function () {
+  it("###", function () {
+    const exprs = parser("###", false);
+    expect(exprs).toEqual([{ type: "seperator", value: [] }]);
+  });
+
+  it("###", function () {
     const exprs = parser("###\n", false);
-    expect(exprs).toBe({ type: "seperator", value: [] });
+    expect(exprs).toEqual([{ type: "seperator", value: [] }]);
+  });
+
+  it("with comments 'ddddd'", function () {
+    const exprs = parser("###ddddd\n", false);
+    expect(exprs).toEqual([{ type: "seperator", value: ["ddddd"] }]);
+  });
+
+  it("with comments 'd'", function () {
+    const exprs = parser("###d\n", false);
+    expect(exprs).toEqual([{ type: "seperator", value: ["d"] }]);
+  });
+
+  it("with comments '   ddddd     '", function () {
+    const exprs = parser("###   ddddd     \n", false);
+    expect(exprs).toEqual([{ type: "seperator", value: ["ddddd"] }]);
+  });
+
+  it("with comments '   \tddddd     '", function () {
+    const exprs = parser("###   \tddddd     \n", false);
+    expect(exprs).toEqual([{ type: "seperator", value: ["ddddd"] }]);
+  });
+});
+
+describe("parser meta type prompt", function () {
+  it("with", function () {
+    const exprs = parser("# @prompt\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@prompt"] }]);
+  });
+
+  it("with     ", function () {
+    const exprs = parser("# @prompt     \n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@prompt"] }]);
+  });
+
+  it("with aa", function () {
+    const exprs = parser("# @prompt aa\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@prompt", "aa"] }]);
+  });
+
+  it("with aa description info", function () {
+    const exprs = parser("# @prompt aa description info\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@prompt", "aa", "description info"] }]);
+  });
+
+  it("with   aa  description info ", function () {
+    const exprs = parser("#   @prompt   aa  description info \n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@prompt", "aa", "description info"] }]);
+  });
+
+  it("with a b", function () {
+    const exprs = parser("# @prompt a b\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@prompt", "a", "b"] }]);
+  });
+});
+
+describe("parser meta type note", function () {
+  it("with", function () {
+    const exprs = parser("# @note\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@note"] }]);
+  });
+
+  it("with     ", function () {
+    const exprs = parser("# @note     \n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@note"] }]);
+  });
+
+  it("with aa", function () {
+    const exprs = parser("# @note aa\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@note", "aa"] }]);
+  });
+
+  it("with aa description info", function () {
+    const exprs = parser("# @note aa description info\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@note", "aa description info"] }]);
+  });
+
+  it("with   aa  description info ", function () {
+    const exprs = parser("#   @note   aa  description info \n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@note", "aa  description info"] }]);
+  });
+
+  it("with a b", function () {
+    const exprs = parser("# @note a\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@note", "a"] }]);
+  });
+});
+
+describe("parser meta type name", function () {
+  it("with", function () {
+    const exprs = parser("# @name\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@name"] }]);
+  });
+
+  it("with     ", function () {
+    const exprs = parser("# @name     \n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@name"] }]);
+  });
+
+  it("with aa", function () {
+    const exprs = parser("# @name aa\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@name", "aa"] }]);
+  });
+
+  it("with aa description info", function () {
+    const exprs = parser("# @name aa description info\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@name", "aa description info"] }]);
+  });
+
+  it("with   aa  description info ", function () {
+    const exprs = parser("#   @name   aa  description info \n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@name", "aa  description info"] }]);
+  });
+
+  it("with a b", function () {
+    const exprs = parser("# @name a\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@name", "a"] }]);
+  });
+});
+
+describe("parser meta type no redirect", function () {
+  it("with", function () {
+    const exprs = parser("# @no-redirect\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@no-redirect"] }]);
+  });
+
+  it("with     ", function () {
+    const exprs = parser("# @no-redirect     \n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@no-redirect"] }]);
+  });
+
+  it("with aa", function () {
+    const exprs = parser("# @no-redirect aa\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@no-redirect", "aa"] }]);
+  });
+
+  it("with aa description info", function () {
+    const exprs = parser("# @no-redirect aa description info\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@no-redirect", "aa description info"] }]);
+  });
+
+  it("with   aa  description info ", function () {
+    const exprs = parser("#   @no-redirect   aa  description info \n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@no-redirect", "aa  description info"] }]);
+  });
+
+  it("with a b", function () {
+    const exprs = parser("# @no-redirect a\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@no-redirect", "a"] }]);
+  });
+});
+
+describe("parser meta type no cookie jar", function () {
+  it("with", function () {
+    const exprs = parser("# @no-cookie-jar\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@no-cookie-jar"] }]);
+  });
+
+  it("with     ", function () {
+    const exprs = parser("# @no-cookie-jar     \n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@no-cookie-jar"] }]);
+  });
+
+  it("with aa", function () {
+    const exprs = parser("# @no-cookie-jar aa\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@no-cookie-jar", "aa"] }]);
+  });
+
+  it("with aa description info", function () {
+    const exprs = parser("# @no-cookie-jar aa description info\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@no-cookie-jar", "aa description info"] }]);
+  });
+
+  it("with   aa  description info ", function () {
+    const exprs = parser("#   @no-cookie-jar   aa  description info \n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@no-cookie-jar", "aa  description info"] }]);
+  });
+
+  it("with a b", function () {
+    const exprs = parser("# @no-cookie-jar a\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@no-cookie-jar", "a"] }]);
+  });
+});
+
+describe("parser meta type comment", function () {
+  it("with#", function () {
+    const exprs = parser("##\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@comment", "#"] }]);
+  });
+
+  it("with #", function () {
+    const exprs = parser("# #\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@comment", " ", "#"] }]);
+  });
+
+  it("with", function () {
+    const exprs = parser("#\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@comment"] }]);
+  });
+
+  it("with @names", function () {
+    const exprs = parser("# @names\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@comment", " ", "@names"] }]);
+  });
+
+  it("with     ", function () {
+    const exprs = parser("# @name     \n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@name"] }]);
+  });
+
+  it("with aa", function () {
+    const exprs = parser("# @name aa\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@name", "aa"] }]);
+  });
+
+  it("with aa description info", function () {
+    const exprs = parser("# @name aa description info\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@name", "aa description info"] }]);
+  });
+
+  it("with   aa  description info ", function () {
+    const exprs = parser("#   @name   aa  description info \n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@name", "aa  description info"] }]);
+  });
+
+  it("with a b", function () {
+    const exprs = parser("# @name a\n", false);
+    expect(exprs).toEqual([{ type: "meta", value: ["@name", "a"] }]);
   });
 });
